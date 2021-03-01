fa68a55922f1b4ea29bc7f660f323903d10bbfa5
Ricky Ng-Adam <rngadam@gmail.com>
2021-03-01 04:41:37 -0500

    with doc

---
+++ README.md
@@ -0,0 +1,22 @@
+
+## what is it
+
+Custom generation of PDF for the member contract with fillable field
+
+## reqs
+
+needs Docker installed and running 
+
+## install
+
+```
+./install.sh
+```
+
+## run
+
+```
+./run.sh
+```
+
+outputs serment.pdf
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 # https://github.com/nodejs/docker-node
-docker run -it --rm --name my-running-script -v "$PWD":/usr/src/app -w /usr/src/app node:14 node run.js
+docker run -it --rm --name generate -v "$PWD":/usr/src/app -w /usr/src/app node:14 node generate.js
